4517bba7e484419523d4635002df7c039e201b6b
Nabe <yudai.0702wa@gmail.com>
2025-07-02 19:20:01 +0900

    feat: Integrate Firebase and add core plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:motimate/app.dart';
+import 'package:motimate/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
